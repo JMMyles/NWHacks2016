6718f92298169f83f715a629c9009a641ad5da15
JMMyles <JosephMMyles@gmail.com>
2016-02-27 22:26:54 -0800

    Added code to deal with financial information

---
+++ FinancialData.py
@@ -0,0 +1,14 @@
+import urllib.request
+import re
+
+def get_stock_quote(symbol):
+	base_url = 'http://finance.google.com/finance?q='
+	content = urllib.request.urlopen(base_url + symbol).read()
+	m = re.search('id="ref_694653_l".*?>(.*?)<', content)
+	if m:
+		quote = m.group(1)
+	else:
+		quote = 'no quote available for: ' + symbol
+	return quote
+
+print(get_stock_quote('NASDAQ'))
