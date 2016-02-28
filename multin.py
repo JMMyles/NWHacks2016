6bf5db4b0f5c20c5e4dc430fd0a269739682e850
TylerJamesMalloy <tylerjmalloy@gmail.com>
2016-02-28 07:18:18 -0800

    working??

--- multin.py
+++ multin.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from pylab import plot, show, xlabel, ylabel
 import re
 import numpy as np
+import math
+from sklearn import preprocessing, metrics, cross_validation
 
 
 HouseFile = open('HousingData.csv','r')
@@ -29,38 +31,12 @@ def make_House(zip_code, beds, baths,sqr_feet,price):
     house.price = price
     return house
 
+
 def __repr__(self):
     return "<Test zip:%s beds:%s baths:%s sqr_feet:%s price:%s>" % (self.zip_code, self.beds,self.baths,self.sqr_feet,self.price)
 
 def __str__(self):
     return "zip:%s beds:%s baths:%s sqr_feet:%s price:%s" % (self.zip_code, self.beds,self.baths,self.sqr_feet,self.price)
-
-#Evaluate the linear regression
-
-def feature_normalize(X):
-    '''
-    Returns a normalized version of X where
-    the mean value of each feature is 0 and the standard deviation
-    is 1. This is often a good preprocessing step to do when
-    working with learning algorithms.
-    '''
-    mean_r = []
-    std_r = []
-
-    X_norm = X
-    #np.insert(X_norm, 1, 1, axis=1)
-
-    n_c = X.shape[1]
-    for i in range(n_c):
-        m = mean(X[:, i])
-        s = std(X[:, i])
-        mean_r.append(m)
-        std_r.append(s)
-        #print(s)
-        #print((X_norm[:, i] - m))
-        X_norm[:, i] = (X_norm[:, i] - m) / s
-
-    return X_norm, mean_r, std_r
 
 def compute_cost(X, y, theta):
     '''
@@ -68,11 +44,8 @@ def compute_cost(X, y, theta):
     '''
     #Number of training samples
     m = y.size
-
     predictions = X.dot(theta)
-
     sqErrors = (predictions - y)
-
     J = (1.0 / (2 * m)) * sqErrors.T.dot(sqErrors)
 
     return J
@@ -99,7 +72,6 @@ def gradient_descent(X, y, theta, alpha, num_iters):
             theta[it][0] = theta[it][0] - alpha * (1.0 / m) * errors_x1.sum()
 
         J_history[i, 0] = compute_cost(X, y, theta)
-        
     return(theta, J_history)
 
 
@@ -108,13 +80,12 @@ for line in HouseFile:
     matchObj = re.match( r'\d+ \w+ \w+,\w+,(\w+),\w+,(\d+),(\d+),(\d+),\w+,\w+ \w+ \d+ \d+:\d+:\d+ \w+ \d+,(\d+)', line, re.M|re.I)
 
     if matchObj:
-        if matchObj.group(1).isdigit():
-            house = make_House(matchObj.group(1),matchObj.group(2),matchObj.group(3),matchObj.group(4),matchObj.group(5))
-            new_value = np.matrix([float(matchObj.group(1)),float(matchObj.group(2)),float(matchObj.group(3)),float(matchObj.group(4))])
-            info_list = np.concatenate((info_list, new_value), axis=0)
-            new_price = np.matrix([float(matchObj.group(5))])
-            price_matrix = np.concatenate((price_matrix, new_price), axis=0)
-            house_list.append(house)
+        house = make_House(matchObj.group(1),matchObj.group(2),matchObj.group(3),matchObj.group(4),matchObj.group(5))
+        new_value = np.matrix([float(matchObj.group(1)),float(matchObj.group(2)),float(matchObj.group(3)),float(matchObj.group(4))])
+        info_list = np.concatenate((info_list, new_value), axis=0)
+        new_price = np.matrix([float(matchObj.group(5))])
+        price_matrix = np.concatenate((price_matrix, new_price), axis=0)
+        house_list.append(house)
 
 X = info_list.transpose()
 y = price_matrix
@@ -125,18 +96,19 @@ m = y.size
 y.shape = (m, 1)
 
 #Scale features and set them to zero mean
-x, mean_r, std_r = feature_normalize(X)
+X_normalized = preprocessing.normalize(X, norm='l2')
 #Add a column of ones to X (interception data)
-it = ones(shape=(m, 4))
-it[:, 0:4] = x.transpose()
+it = np.ones((m,5))
+it[:,:-1] = X_normalized.transpose()
 
 #Some gradient descent settings
-iterations = 100
-alpha = 0.01
+iterations = 1
+alpha = 1
 
 #Init Theta and Run Gradient Descent
-theta = zeros(shape=(4, 1))
+theta = zeros(shape=(5, 1))
 
 Theta, J_history = gradient_descent(it, y, theta, alpha, iterations)
-
+print(Theta)
+print(J_history)
 
